cd243e25a1eedf6be7f7b3c4a6b69cb934d2dc1e
Milena Tselikina <milenochka.andreevna@mail.ru>
2020-10-07 14:56:58 +0400

    Fix Main, mutable elements and delete setup

--- src/main/scala/com/payment/Main.scala
+++ src/main/scala/com/payment/Main.scala
@@ -6,28 +6,24 @@ import akka.stream.Materializer
 
 import scala.concurrent.ExecutionContext.Implicits.global
 
-object MainClass {
+object OrchestratorSystem {
   def apply(): Behavior[String] =
     Behaviors.setup { context =>
-      Behaviors.receiveMessage {
-        case "start" =>
-          val paymentChecker = context.spawn(PaymentChecker(), "payment-checker")
-          val directory = context.system.settings.config.getString("akka.reading.catalog")
-          val mask = context.system.settings.config.getString("akka.reading.mask")
-          val delimiter = context.system.settings.config.getString("akka.reading.delimiter")
-          val maximumFrameLength = context.system.settings.config.getInt("akka.reading.maximumFrameLength")
-          val paymentsReader =
-            new PaymentsReader(paymentChecker, directory, mask.r, delimiter, maximumFrameLength)(
-              Materializer.matFromSystem(context.system.classicSystem))
-          paymentsReader.readPayments().andThen {
-            case _ => context.system.terminate()
-          }
-          Behaviors.same
+      val paymentChecker = context.spawn(PaymentChecker(), "payment-checker")
+      val directory = context.system.settings.config.getString("akka.reading.catalog")
+      val mask = context.system.settings.config.getString("akka.reading.mask")
+      val delimiter = context.system.settings.config.getString("akka.reading.delimiter")
+      val maximumFrameLength = context.system.settings.config.getInt("akka.reading.maximumFrameLength")
+      val paymentsReader =
+        new PaymentsReader(paymentChecker, directory, mask.r, delimiter, maximumFrameLength)(
+          Materializer.matFromSystem(context.system.classicSystem))
+      paymentsReader.readPayments().andThen {
+        case _ => context.system.terminate()
       }
+      Behaviors.same
     }
 }
 
 object Main extends App {
-  val mySystem = ActorSystem(MainClass(), "PaymentsSystem")
-  mySystem ! "start"
+  val mySystem = ActorSystem(OrchestratorSystem(), "PaymentsSystem")
 }
--- src/main/scala/com/payment/PaymentParticipant.scala
+++ src/main/scala/com/payment/PaymentParticipant.scala
@@ -6,32 +6,37 @@ import akka.actor.typed.{ActorRef, Behavior}
 object PaymentParticipant {
 
   sealed trait PaymentCommand
+
   case class Payment(sign: PaymentSign, value: Long, participant: ActorRef[PaymentCommand]) extends PaymentCommand
+
   case class StopPayment(value: Long) extends PaymentCommand
 
   sealed trait PaymentSign
+
   final case object PlusSign extends PaymentSign
+
   final case object MinusSign extends PaymentSign
 
-  def apply(name: String, balance: Long): Behavior[PaymentCommand] =
+  def apply(name: String, balance: Long): Behavior[PaymentCommand] = {
+    paymentProcess(name, balance)
+  }
+
+  def paymentProcess(name: String, balance: Long): Behavior[PaymentCommand] =
     Behaviors.setup { context =>
-      var balanceUser = balance
-      Behaviors.receiveMessage{
-        case Payment(MinusSign, value, participant) if value > balanceUser =>
+      Behaviors.receiveMessage {
+        case Payment(MinusSign, value, participant) if value > balance =>
           context.log.error(s"User lacks balance $name! Rolling back an operation.")
           participant ! StopPayment(value)
-          Behaviors.same
+          Behaviors.stopped
         case Payment(MinusSign, value, _) =>
-          balanceUser -= value
-          context.log.info(s"Transfer from $name: $value. Balance: $balanceUser.")
+          context.log.info(s"Transfer from $name: $value. Balance: ${balance - value}.")
           Behaviors.stopped
         case Payment(PlusSign, value, _) =>
-          balanceUser += value
+          val balanceUser = balance + value
           context.log.info(s"Transfer to $name: $value. Balance: $balanceUser.")
-          Behaviors.same
+          paymentProcess(name, balanceUser)
         case StopPayment(value) =>
-          balanceUser -= value
-          context.log.info(s"Canceling a transfer to $name: $value. Balance: $balanceUser.")
+          context.log.info(s"Canceling a transfer to $name: $value. Balance: ${balance - value}.")
           Behaviors.stopped
       }
     }
